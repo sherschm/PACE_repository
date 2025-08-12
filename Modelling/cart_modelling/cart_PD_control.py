5b98edbed47d03e3a444fd149e3001e1e2065ad1
Herschmann, Sam <sam.herschmann@ukaea.uk>
2025-08-12 15:43:26 +0100

    working animations

--- Modelling/cart_modelling/cart_PD_control.py
+++ Modelling/cart_modelling/cart_PD_control.py
@@ -5,7 +5,6 @@ from cart_plotting import make_animation_with_force
 import os
 file_directory = os.path.dirname(os.path.abspath(__file__))
 
-
 # --- System Parameters ---
 m = 10  # kg
 A = np.array([[0, 1],
@@ -14,32 +13,34 @@ B = np.array([[0],
               [1/m]])
 
 # Desired motion
-cmnd = 7.0      # desired position
+cmnd = 5.0      # desired position
 cmnd_dot = 0.0  # desired velocity
 
 # PD gains
 K_p, K_d = 100, 60
 
 # Control input function
-def u(q, t):
-    return K_p * (cmnd - q[0]) + K_d * (cmnd_dot - q[1])
+def u(t,x):
+    force = K_p * (cmnd - x[0]) + K_d * (cmnd_dot - x[1])
+    return np.array([force])
 
 # Dynamics function
-def qdot(t, q):
-    return (A @ q + B.flatten() * u(q, t))
+def xdot(t, x):
+    dx = A @ x + B @ u(t,x)
+    return dx.flatten()
 
 # Initial conditions & simulation parameters
 t_span = (0, 10)   # time range
-q0 = [0.0, 0.0]    # initial state: [x, xdot]
+x0 = np.array([0.0, 0.0])    # initial state: [x, xdot]
 
 # Solve the ODE
-solution = solve_ivp(qdot, t_span, q0, t_eval=np.linspace(*t_span, 400))
+solution = solve_ivp(xdot, t_span, x0, t_eval=np.linspace(*t_span, 400))
 
 # Extract data
 t = solution.t
 x = solution.y[0]
 xdot = solution.y[1]
-F_vec = np.array([u([xi, vi], ti) for xi, vi, ti in zip(x, xdot, t)])
+F = np.array([u(ti,[xi, vi])[0] for xi, vi, ti in zip(x, xdot, t)])
 
 # --- Plot results ---
 plt.figure()
@@ -48,6 +49,14 @@ plt.plot(t, xdot, label="dx/dt (m/s)")
 plt.xlabel("time (s)")
 plt.legend()
 plt.grid(True)
-plt.savefig("response_plot.png", dpi=150)
+plt.savefig(str(file_directory)+"/response_plot.png", dpi=150)
 
-make_animation_with_force(x_sol=x, F_sol=F_vec, t_vec=t, file_name=str(file_directory)+"/Cart_simulation.gif")
+plt.figure()
+plt.plot(t, F)
+plt.xlabel("time (s)")
+plt.ylabel("Force (N)")
+plt.grid(True)
+plt.savefig(str(file_directory)+"/force_plot.png", dpi=150)
+
+# --- Generate visualisation of the cart simulation! ---
+make_animation_with_force(x_sol=x, F_sol=F, t_vec=t, file_name=str(file_directory)+"/Cart_simulation.gif")
--- Modelling/cart_modelling/cart_model.py
+++ Modelling/cart_modelling/cart_model.py
@@ -12,26 +12,28 @@ A = np.array([[0, 1],
 B = np.array([[0],
               [1/m]])
 
-# Control input
-def u(q, t):
-    return 1
+# Define control input
+def u(t,x):
+    force = 15*np.sin(1.2*t) #Newton
+    return np.array([force])
 
 # Dynamics function
-def qdot(t, q):
-    return (A @ q + B.flatten() * u(q, t))
+def xdot(t, x):
+    dx = A @ x + B @ u(t,x)
+    return dx.flatten()
 
 # Initial conditions & simulation parameters
 t_span = (0, 10)   # time range
-q0 = [0.0, 0.0]    # initial state: [x, xdot]
+x0 = np.array([0.0, 0.0])    # initial state: [x, xdot]
 
 # Solve the ODE
-solution = solve_ivp(qdot, t_span, q0, t_eval=np.linspace(*t_span, 400))
+solution = solve_ivp(xdot, t_span, x0, t_eval=np.linspace(*t_span, 400))
 
-# Extract data
+# Extract simulation results and visualise
 t = solution.t
 x = solution.y[0]
 xdot = solution.y[1]
-F_vec = np.array([u([xi, vi], ti) for xi, vi, ti in zip(x, xdot, t)])
+F = np.array([u(ti,[xi, vi])[0] for xi, vi, ti in zip(x, xdot, t)])
 
 # --- Plot results ---
 plt.figure()
@@ -42,5 +44,12 @@ plt.legend()
 plt.grid(True)
 plt.savefig(str(file_directory)+"/response_plot.png", dpi=150)
 
+plt.figure()
+plt.plot(t, F)
+plt.xlabel("time (s)")
+plt.ylabel("Force (N)")
+plt.grid(True)
+plt.savefig(str(file_directory)+"/force_plot.png", dpi=150)
+
 # --- Generate visualisation of the cart simulation! ---
-make_animation_with_force(x_sol=x, F_sol=F_vec, t_vec=t, file_name=str(file_directory)+"/Cart_simulation.gif")
+make_animation_with_force(x_sol=x, F_sol=F, t_vec=t, file_name=str(file_directory)+"/Cart_simulation.gif")
